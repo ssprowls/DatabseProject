809b5773e05d06dec95ff83ce0e6ef887027d587
evilaaron11 <aaron.m.a@comcast.net>
2017-03-14 16:55:35 -0700

    linked to database nd queries based on username or name

--- SearchUserView.java
+++ SearchUserView.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.Font;
+import java.sql.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -29,6 +30,7 @@ public class SearchUserView extends JLayeredPane
     // instance variables - replace the example below with your own
     private JLabel backgroundLabel;
     private JLabel titleLabel;
+    private JLabel statusLabel;
     private JLabel nameLabel;
     private JLabel userLabel;
     private JTextField jtfUser;
@@ -45,6 +47,7 @@ public class SearchUserView extends JLayeredPane
         backgroundLabel = new JLabel();
         nameLabel = new JLabel("Name");
         userLabel = new JLabel("Username");
+        statusLabel = new JLabel();
         jtfUser = new JTextField(20);
         jtfName = new JTextField(20);
         search = new JButton("Search");   
@@ -54,14 +57,16 @@ public class SearchUserView extends JLayeredPane
         backgroundLabel.setBounds(0,0,800,600);
         backgroundLabel.setIcon(icon);
         
+        statusLabel.setBounds(280, 290, 80, 30);
+        statusLabel.setForeground(Color.WHITE);
         userLabel.setBounds(200, 200, 80, 30);        
         userLabel.setForeground(Color.WHITE);
-        nameLabel.setBounds(200, 260, 80, 30);
+        nameLabel.setBounds(200, 230, 80, 30);
         nameLabel.setForeground(Color.WHITE);
         jtfUser.setBounds(280, 205, 200, 20);
-        jtfName.setBounds(280, 265, 200, 20);
-        search.setBounds(380, 325, 100, 20);
-        back.setBounds(280, 325, 100, 20);
+        jtfName.setBounds(280, 235, 200, 20);
+        search.setBounds(380, 265, 100, 20);
+        back.setBounds(280, 265, 100, 20);
         titleLabel.setBounds(280, 175, 200, 30);
         titleLabel.setForeground(Color.WHITE);
         titleLabel.setFont(new Font("Courier", Font.PLAIN, 20));
@@ -73,9 +78,93 @@ public class SearchUserView extends JLayeredPane
         add(search, new Integer(1));
         add(back, new Integer(1));
         add(titleLabel, new Integer(1));
+        add(statusLabel, new Integer(1));
         add(backgroundLabel, new Integer(1));
+        search.addActionListener(new SearchListener());
 
     }
+    private class SearchListener implements ActionListener
+    {
 
-    
+    @Override
+        public void actionPerformed(ActionEvent ae) 
+        {     
+            // Process data
+            //System.out.println(jtfUser.getText());
+            if (jtfName.getText().length() == 0) {
+                connect(jtfUser.getText());
+            } else {
+                conName(jtfName.getText());
+
+            }
+        }
+    }
+    public void connect(String toSearch) {
+        boolean hadNone = true;
+        int userID = 0;
+        try
+            {  
+                Class.forName("com.mysql.jdbc.Driver");  
+                Connection con=DriverManager.getConnection(  
+                        "jdbc:mysql://calteccomputers.com/caltec5_365", "caltec5_team", "cheddar");  
+                Statement stmt=con.createStatement();  
+                ResultSet rs=stmt.executeQuery("SELECT id FROM Users WHERE username LIKE '%" + toSearch + "%'");  
+                while(rs.next()) 
+                {
+                        System.out.println(rs.getInt(1));//+"  "+rs.getString(2)+"  "+rs.getString(3));
+                        userID = rs.getInt(1);
+                        hadNone = false;
+                }
+                if (hadNone)
+                {
+                    statusLabel.setText("No match found.");
+                } else {
+                    JFrame frame = (JFrame) SwingUtilities.getRoot(jtfName);
+                    frame.getContentPane().removeAll();           
+                    frame.getContentPane().add(new UserView());//busID));
+                    frame.getContentPane().validate();
+                    frame.getContentPane().repaint();
+                }
+                
+                con.close();  
+            }
+            catch(Exception e)
+            { 
+                System.out.println(e);
+            }
+    }
+    public void conName(String toSearch) {
+        boolean hadNone = true;
+        int userID = 0;
+        try
+            {  
+                Class.forName("com.mysql.jdbc.Driver");  
+                Connection con=DriverManager.getConnection(  
+                        "jdbc:mysql://calteccomputers.com/caltec5_365", "caltec5_team", "cheddar");  
+                Statement stmt=con.createStatement();  
+                ResultSet rs=stmt.executeQuery("SELECT id FROM Users WHERE name LIKE '%" + toSearch + "%'");  
+                while(rs.next()) 
+                {
+                        System.out.println(rs.getInt(1));//+"  "+rs.getString(2)+"  "+rs.getString(3));
+                        userID = rs.getInt(1);
+                        hadNone = false;
+                }
+                if (hadNone)
+                {
+                    statusLabel.setText("No match found.");
+                } else {
+                    JFrame frame = (JFrame) SwingUtilities.getRoot(jtfName);
+                    frame.getContentPane().removeAll();           
+                    frame.getContentPane().add(new UserView());//busID));
+                    frame.getContentPane().validate();
+                    frame.getContentPane().repaint();
+                }
+                
+                con.close();  
+            }
+            catch(Exception e)
+            { 
+                System.out.println(e);
+            }
+    }
 }

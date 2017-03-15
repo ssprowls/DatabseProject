ba9e7446a4750a382016ade65b3d557dae9f13bb
root <agterris@yahoo.com>
2017-03-14 20:13:17 -0700

    Replaced Table with Labels

--- BusView.java
+++ BusView.java
@@ -10,7 +10,6 @@ import java.sql.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTable;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
@@ -34,16 +33,28 @@ public class BusView extends JLayeredPane
     private JLabel titleLabel;
     private JLabel descriptionLabel;
     private JLabel ratingLabel;
+    private JLabel reviewName1;
+    private JLabel reviewName2;
+    private JLabel reviewName3;
+    private JLabel reviewName4;
+    private JLabel reviewText1;
+    private JLabel reviewText2;
+    private JLabel reviewText3;
+    private JLabel reviewText4;
+    private JLabel reviewRating1;
+    private JLabel reviewRating2;
+    private JLabel reviewRating3;
+    private JLabel reviewRating4;
     private JButton back;
-    private JTable table;
     private JButton write;
     private int bus_id;
     private String businessName;
     private String businessDescription;
     private float businessAvgRating; 
     private Connection con;
-    private String[] columnNames = {"Name", "Review", "Rating", "Date"};
-    private String[][] tableData = new String[10][];
+    private String[] names = new String[4];
+    private String[] reviews = new String[4];
+    private String[] ratings = new String[4];
 
     /**
      * Constructor for objects of class SearchBusView
@@ -59,13 +70,65 @@ public class BusView extends JLayeredPane
         ratingLabel = new JLabel("Average Rating: " + Float.toString(businessAvgRating));
         write = new JButton("Write Review");   
         back = new JButton("Back");
-        table = new JTable(tableData, columnNames);
+        
+        //Review List
+        reviewName1 = new JLabel(names[0]);
+        reviewName2 = new JLabel(names[1]);
+        reviewName3 = new JLabel(names[2]);
+        reviewName4 = new JLabel(names[3]);
+        reviewText1 = new JLabel(reviews[0]);
+        reviewText2 = new JLabel(reviews[1]);
+        reviewText3 = new JLabel(reviews[2]);
+        reviewText4 = new JLabel(reviews[3]);
+        reviewRating1 = new JLabel("Rating: " + ratings[0] + " / 5");
+        reviewRating2 = new JLabel("Rating: " + ratings[1] + " / 5");
+        reviewRating3 = new JLabel("Rating: " + ratings[2] + " / 5");
+        reviewRating4 = new JLabel("Rating: " + ratings[3] + " / 5");
         
         ImageIcon icon = new ImageIcon("bground5.jpg");
         backgroundLabel.setBounds(0,0,800,600);
         backgroundLabel.setIcon(icon);
      
-        //table.setBounds(20, 100, 300, 300);
+        
+        //Review List
+        reviewName1.setBounds(20, 100, 200, 30);
+        reviewName1.setForeground(Color.WHITE);
+        reviewName1.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText1.setBounds(20, 130, 700, 30);
+        reviewText1.setForeground(Color.WHITE);
+        reviewText1.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating1.setBounds(660, 100, 200, 30);
+        reviewRating1.setForeground(Color.WHITE);
+        reviewRating1.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewName2.setBounds(20, 180, 200, 30);
+        reviewName2.setForeground(Color.WHITE);
+        reviewName2.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText2.setBounds(20, 210, 700, 30);
+        reviewText2.setForeground(Color.WHITE);
+        reviewText2.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating2.setBounds(660, 180, 200, 30);
+        reviewRating2.setForeground(Color.WHITE);
+        reviewRating2.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewName3.setBounds(20, 260, 200, 30);
+        reviewName3.setForeground(Color.WHITE);
+        reviewName3.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText3.setBounds(20, 290, 700, 30);
+        reviewText3.setForeground(Color.WHITE);
+        reviewText3.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating3.setBounds(660, 260, 200, 30);
+        reviewRating3.setForeground(Color.WHITE);
+        reviewRating3.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewName4.setBounds(20, 340, 200, 30);
+        reviewName4.setForeground(Color.WHITE);
+        reviewName4.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText4.setBounds(20, 370, 700, 30);
+        reviewText4.setForeground(Color.WHITE);
+        reviewText4.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating4.setBounds(660, 340, 200, 30);
+        reviewRating4.setForeground(Color.WHITE);
+        reviewRating4.setFont(new Font("Courier", Font.PLAIN, 16));
+        
+        //Navigation Buttons
         write.setBounds(660, 520, 100, 20);
         back.setBounds(20, 520, 100, 20);
         
@@ -79,9 +142,21 @@ public class BusView extends JLayeredPane
         ratingLabel.setForeground(Color.WHITE);
         ratingLabel.setFont(new Font("Courier", Font.PLAIN, 14));
         
+        
+        add(reviewName1, new Integer(1));
+        add(reviewName2, new Integer(1));
+        add(reviewName3, new Integer(1));
+        add(reviewName4, new Integer(1));
+        add(reviewText1, new Integer(1));
+        add(reviewText2, new Integer(1));
+        add(reviewText3, new Integer(1));
+        add(reviewText4, new Integer(1));
+        add(reviewRating1, new Integer(1));
+        add(reviewRating2, new Integer(1));
+        add(reviewRating3, new Integer(1));
+        add(reviewRating4, new Integer(1));
         add(ratingLabel, new Integer(1));
         add(write, new Integer(1));
-        //add(table, new Integer(1));
         add(back, new Integer(1));
         add(titleLabel, new Integer(1));
         add(descriptionLabel, new Integer(1));
@@ -142,14 +217,15 @@ public class BusView extends JLayeredPane
             }
             
             //Reviews
-            String[] row;
             int count = 0;
             Statement reviewsStatement = con.createStatement();  
-            ResultSet reviewsResults = avgRatStatement.executeQuery("SELECT name, review, rating, date FROM Reviews JOIN Users ON Users.id = user_id WHERE bus_id = " + bus_id + " ORDER BY date DESC");  
+            ResultSet reviewsResults = avgRatStatement.executeQuery("SELECT name, review, rating FROM Reviews JOIN Users ON Users.id = user_id WHERE bus_id = " + bus_id + " ORDER BY date DESC LIMIT 4");  
             while(reviewsResults.next()) 
             {
-                row = new String[]{reviewsResults.getString(1), reviewsResults.getString(2), reviewsResults.getString(3), reviewsResults.getString(4)};
-                tableData[count++] = row;
+                names[count] = reviewsResults.getString(1);
+                reviews[count] = reviewsResults.getString(2);
+                ratings[count] = reviewsResults.getString(3);
+                count++;
             }
             con.close();  
         }
--- UserView.java
+++ UserView.java
@@ -31,55 +31,130 @@ public class UserView extends JLayeredPane
     // instance variables - replace the example below with your own
     private JLabel backgroundLabel;
     private JLabel titleLabel;
-    //private JLabel nameLabel;
-    private JLabel busLabel;
-    private JTextField jtfBus;
-    //private JTextField jtfName;
+    private JLabel descriptionLabel;
+    private JLabel reviewName1;
+    private JLabel reviewName2;
+    private JLabel reviewName3;
+    private JLabel reviewName4;
+    private JLabel reviewText1;
+    private JLabel reviewText2;
+    private JLabel reviewText3;
+    private JLabel reviewText4;
+    private JLabel reviewRating1;
+    private JLabel reviewRating2;
+    private JLabel reviewRating3;
+    private JLabel reviewRating4;
     private JButton back;
-    private JButton search;
+    private JButton write;
     private int user_id;
+    private String userName;
+    private String userEmail;
+    private Connection con;
+    private String[] names = new String[4];
+    private String[] reviews = new String[4];
+    private String[] ratings = new String[4];
 
     /**
      * Constructor for objects of class SearchBusView
      */
-    public UserView()
+    public UserView(int id)
     {
+        this.user_id = id;
+        getUserData(user_id);
         // initialise instance variables
-        titleLabel = new JLabel("Search Businesses");
+        titleLabel = new JLabel(userName);
+        descriptionLabel = new JLabel(businessDescription);
         backgroundLabel = new JLabel();
-        //nameLabel = new JLabel("Name");
-        busLabel = new JLabel("Business");
-        jtfBus = new JTextField(20);
-        //jtfName = new JTextField(20);
-        search = new JButton("Search");   
+        write = new JButton("Write Review");   
         back = new JButton("Back");
+        
+        //Review List
+        reviewName1 = new JLabel(names[0]);
+        reviewName2 = new JLabel(names[1]);
+        reviewName3 = new JLabel(names[2]);
+        reviewName4 = new JLabel(names[3]);
+        reviewText1 = new JLabel(reviews[0]);
+        reviewText2 = new JLabel(reviews[1]);
+        reviewText3 = new JLabel(reviews[2]);
+        reviewText4 = new JLabel(reviews[3]);
+        reviewRating1 = new JLabel("Rating: " + ratings[0] + " / 5");
+        reviewRating2 = new JLabel("Rating: " + ratings[1] + " / 5");
+        reviewRating3 = new JLabel("Rating: " + ratings[2] + " / 5");
+        reviewRating4 = new JLabel("Rating: " + ratings[3] + " / 5");
         
         ImageIcon icon = new ImageIcon("bground5.jpg");
         backgroundLabel.setBounds(0,0,800,600);
         backgroundLabel.setIcon(icon);
+     
         
-        busLabel.setBounds(200, 200, 80, 30);        
-        busLabel.setForeground(Color.WHITE);
-        //nameLabel.setBounds(200, 230, 80, 30);
-        //nameLabel.setForeground(Color.WHITE);
-        jtfBus.setBounds(280, 205, 200, 20);
-        //jtfName.setBounds(280, 235, 200, 20);
-        search.setBounds(380, 265, 100, 20);
-        back.setBounds(280, 265, 100, 20);
-        titleLabel.setBounds(280, 175, 200, 30);
+        //Review List
+        reviewName1.setBounds(20, 100, 200, 30);
+        reviewName1.setForeground(Color.WHITE);
+        reviewName1.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText1.setBounds(20, 130, 700, 30);
+        reviewText1.setForeground(Color.WHITE);
+        reviewText1.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating1.setBounds(660, 100, 200, 30);
+        reviewRating1.setForeground(Color.WHITE);
+        reviewRating1.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewName2.setBounds(20, 180, 200, 30);
+        reviewName2.setForeground(Color.WHITE);
+        reviewName2.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText2.setBounds(20, 210, 700, 30);
+        reviewText2.setForeground(Color.WHITE);
+        reviewText2.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating2.setBounds(660, 180, 200, 30);
+        reviewRating2.setForeground(Color.WHITE);
+        reviewRating2.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewName3.setBounds(20, 260, 200, 30);
+        reviewName3.setForeground(Color.WHITE);
+        reviewName3.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText3.setBounds(20, 290, 700, 30);
+        reviewText3.setForeground(Color.WHITE);
+        reviewText3.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating3.setBounds(660, 260, 200, 30);
+        reviewRating3.setForeground(Color.WHITE);
+        reviewRating3.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewName4.setBounds(20, 340, 200, 30);
+        reviewName4.setForeground(Color.WHITE);
+        reviewName4.setFont(new Font("Courier", Font.PLAIN, 16));
+        reviewText4.setBounds(20, 370, 700, 30);
+        reviewText4.setForeground(Color.WHITE);
+        reviewText4.setFont(new Font("Courier", Font.PLAIN, 12));
+        reviewRating4.setBounds(660, 340, 200, 30);
+        reviewRating4.setForeground(Color.WHITE);
+        reviewRating4.setFont(new Font("Courier", Font.PLAIN, 16));
+        
+        //Navigation Buttons
+        write.setBounds(660, 520, 100, 20);
+        back.setBounds(20, 520, 100, 20);
+        
+        titleLabel.setBounds(20, 20, 200, 30);
         titleLabel.setForeground(Color.WHITE);
         titleLabel.setFont(new Font("Courier", Font.PLAIN, 20));
+        descriptionLabel.setBounds(20, 50, 700, 30);
+        descriptionLabel.setForeground(Color.WHITE);
+        descriptionLabel.setFont(new Font("Courier", Font.PLAIN, 14));
         
-        add(busLabel, new Integer(1));
-        //add(nameLabel, new Integer(1));
-        add(jtfBus, new Integer(1));
-        //add(jtfName, new Integer(1));
-        add(search, new Integer(1));
+        add(reviewName1, new Integer(1));
+        add(reviewName2, new Integer(1));
+        add(reviewName3, new Integer(1));
+        add(reviewName4, new Integer(1));
+        add(reviewText1, new Integer(1));
+        add(reviewText2, new Integer(1));
+        add(reviewText3, new Integer(1));
+        add(reviewText4, new Integer(1));
+        add(reviewRating1, new Integer(1));
+        add(reviewRating2, new Integer(1));
+        add(reviewRating3, new Integer(1));
+        add(reviewRating4, new Integer(1));
+        add(write, new Integer(1));
         add(back, new Integer(1));
         add(titleLabel, new Integer(1));
+        add(descriptionLabel, new Integer(1));
         add(backgroundLabel, new Integer(1));
         back.addActionListener(new BackListener());
-      
+        write.addActionListener(new WriteListener());
     }
     private class BackListener implements ActionListener
     {
@@ -87,28 +162,61 @@ public class UserView extends JLayeredPane
         @Override
         public void actionPerformed(ActionEvent ae) 
         {     
-            // Process data
-            ;
+            JFrame frame = (JFrame) SwingUtilities.getRoot(titleLabel);
+            frame.getContentPane().removeAll();           
+            frame.getContentPane().add(new SearchBusView());
+            frame.getContentPane().validate();
+            frame.getContentPane().repaint();
+                    
+        }
+    }
+    private class WriteListener implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) 
+        {     
+            JFrame frame = (JFrame) SwingUtilities.getRoot(titleLabel);
+            frame.getContentPane().removeAll();           
+            frame.getContentPane().add(new WriteReviewView(bus_id));
+            frame.getContentPane().validate();
+            frame.getContentPane().repaint();
 
         }
     }
-    public void connect(int user_id) {
+    public void getBusinessData(int user_id)
+    {
         try
-            {  
-                Class.forName("com.mysql.jdbc.Driver");  
-                Connection con=DriverManager.getConnection(  
-                        "jdbc:mysql://calteccomputers.com/caltec5_365", "caltec5_team", "cheddar");  
-                Statement stmt=con.createStatement();  
-                ResultSet rs=stmt.executeQuery("select * from Businesses where id = " + user_id);  
-                while(rs.next()) 
-                {
-                    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-                }
-                con.close();  
+        {
+            Class.forName("com.mysql.jdbc.Driver");  
+            con = DriverManager.getConnection("jdbc:mysql://calteccomputers.com/caltec5_365", "caltec5_team", "cheddar");
+            
+            //Business Information
+            Statement infoStatement = con.createStatement();  
+            ResultSet infoResults = infoStatement.executeQuery("SELECT name, email FROM Users WHERE id = " + user_id);  
+            while(infoResults.next()) 
+            {
+                userName = infoResults.getString(1);
+                userEmail = infoResults.getString(2);
             }
-            catch(Exception e)
-            { 
-                System.out.println(e);
+            
+            //Reviews
+            int count = 0;
+            Statement reviewsStatement = con.createStatement();  
+            ResultSet reviewsResults = avgRatStatement.executeQuery("SELECT name, review, rating FROM Reviews JOIN Businesses ON Businesses.id = bus_id WHERE user_id = " + user_id + " ORDER BY date DESC LIMIT 4");  
+            while(reviewsResults.next()) 
+            {
+                names[count] = reviewsResults.getString(1);
+                reviews[count] = reviewsResults.getString(2);
+                ratings[count] = reviewsResults.getString(3);
+                count++;
             }
+            con.close();  
+        }
+       
+        catch(Exception e)
+        { 
+            System.out.println(e);
+        }
     }
 }
